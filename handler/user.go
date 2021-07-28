bc83b57cf74bf6151b7d4ba8dfed9243dc1377ab
akitasummer <644171127@qq.com>
2021-07-28 22:52:24 +0800

    feat: add auth interceptor

--- handler/user.go
+++ handler/user.go
@@ -67,7 +67,48 @@ func SignInHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write([]byte("http://" + r.Host + "/static/view/home.html"))
+	// w.Write([]byte("http://" + r.Host + "/static/view/home.html"))
+	resp := util.RespMsg{
+		Code: 0,
+		Msg:  "OK",
+		Data: struct {
+			Location string
+			Username string
+			Token    string
+		}{
+			Location: "http://" + r.Host + "/static/view/home.html",
+			Username: username,
+			Token:    token,
+		},
+	}
+	w.Write(resp.JSONBytes())
+}
+
+// 查询用户信息
+func UserInfoHandler(w http.ResponseWriter, r *http.Request) {
+	// 1. 解析请求参数
+	r.ParseForm()
+	username := r.Form.Get("username")
+	// token := r.Form.Get("token")
+	// // 2. 验证token是否有效
+	// isValidToken := IsTokenValid(token)
+	// if !isValidToken {
+	// 	w.WriteHeader(http.StatusForbidden)
+	// 	return
+	// }
+	// 3. 查询用户信息
+	user, err := dbplayer.GetUserInfo(username)
+	if err != nil {
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
+	// 4. 组装并且响应用户数据
+	resp := util.RespMsg{
+		Code: 0,
+		Msg:  "OK",
+		Data: user,
+	}
+	w.Write(resp.JSONBytes())
 }
 
 func GenToken(username string) string {
@@ -77,4 +118,11 @@ func GenToken(username string) string {
 	tokenPrefix := util.MD5([]byte(username + ts + "_tokensalt"))
 
 	return tokenPrefix + ts[:8]
+}
+
+func IsTokenValid(token string) bool {
+	if len(token) != 40 {
+		return false
+	}
+	return true
 }
